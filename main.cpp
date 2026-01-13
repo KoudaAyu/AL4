7db00f40341b1f9a23a867e35700f43956dbd3de
AyuKouda <k024g0061@g.neec.ac.jp>
2026-01-13 10:24:34 +0900

    exe用意

--- main.cpp
+++ main.cpp
@@ -1,9 +1,53 @@
 #include <Windows.h>
 
 #include"KamataEngine.h"
+#include"GameScene.h"
+
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
+	KamataEngine::Initialize(L"TD2_3");
+
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	GameScene* gameScene = new GameScene();
+
+	gameScene->Initialize();
+
+	while (true)
+	{
+		if (KamataEngine::Update())
+		{
+			break;
+		}
+
+		#pragma region 更新処理
+
+		// ゲームシーンの更新
+
+		gameScene->Update();
+
+		#pragma endregion 更新処理
+
+		#pragma region 描画処理
+		// 描画前処理
+		dxCommon->PreDraw();
+
+		// ゲームシーンの描画
+		gameScene->Draw();
+
+		//描画終了
+		dxCommon->PostDraw();
+
+		#pragma endregion 描画処理
+	}
+
+	delete gameScene;
+	gameScene = nullptr;
+
+	KamataEngine::Finalize();
+
 	return 0;
 }
